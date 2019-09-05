f08398d422958a76694b18f5318520d4a152fd8b
George Roberts <george.e.roberts@gmail.com>
2019-09-05 10:12:54 +0100

    Add scrollCarousel event handler for left/right arrow

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ console.clear();
 /* ================================ */
 
 const carousel = document.querySelector('.carousel');
+const carouselUl = carousel.querySelector('ul');
+const carouselLi = carousel.querySelector('li');
+const leftArrow = document.querySelector('.arrow.left');
+const rightArrow = document.querySelector('.arrow.right');
 
 // label the images to visually track them, just for convenience,
 
@@ -22,3 +26,19 @@ for (let li of carousel.querySelectorAll('li')) {
 }
 
 // ...your code to make carousel alive!
+
+leftArrow.addEventListener('click', scrollCarousel);
+rightArrow.addEventListener('click', scrollCarousel);
+
+function scrollCarousel(event) {
+  if (event.target.matches('.arrow.left')) {
+    // scroll the ul by the width of the carousel
+    carousel.scrollLeft -= carousel.clientWidth;
+    return;
+  }
+
+  if (event.target.matches('.arrow.right')) {
+    carousel.scrollLeft += carousel.clientWidth;
+    return;
+  }
+}
